85c1a9d8030d46a865faeba13927d8007cfb8242
João Vitor Laurino <joaos.laurino@gmail.com>
2022-12-23 22:58:31 -0300

    final version

--- src/Coin.js
+++ src/Coin.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import React from "react";
 import "./Coin.css";
 
@@ -40,47 +39,4 @@ const Coin = ({ id, name, image, symbol, price, volume, onPriceChange, marketcap
     );
 };
 
-=======
-import React from "react";
-import "./Coin.css";
-
-// Aqui definimos as funções que lidam com o mouse hover
-
-function handleMouseOver(e) {
-    e.currentTarget.classList.add("mouseHover");
-} 
-
-function handleMouseOut(e) {
-    e.currentTarget.classList.remove("mouseHover");
-}
-
-// Aqui efetuamos o mapeamento dos dados recebidos da API 
-
-const Coin = ({ id, name, image, symbol, price, volume, onPriceChange, marketcap }) => {
-    return (
-        <div className="unity" onMouseOver={e => handleMouseOver(e)} onMouseOut={e => handleMouseOut(e)}>
-            <div className="row">
-                <a href={`/charts?id=${id}`} target="_self">
-                    <div className="coin">
-                        <img src={image} alt="crypto" />
-                        <h1 className="name">{name}</h1>
-                        <p className="symbol">{symbol}</p>
-                    </div>
-                </a>
-                <div className="data">
-                    <p className="price">${price}</p>
-                    <p className="volume">${volume.toLocaleString()}</p>
-                    {onPriceChange < 0
-                        ? <p className="percent red">{onPriceChange.toFixed(2)}%</p>
-                        : <p className="percent green">{onPriceChange.toFixed(2)}%</p>
-                    }
-                    <p className="marketcap">${marketcap.toLocaleString()}</p>
-                </div>
-            </div>
-        </div>
-        
-    );
-};
-
->>>>>>> ff6da474b382f8f656dccdb59e11e2f3ab40e6d9
 export default Coin;
